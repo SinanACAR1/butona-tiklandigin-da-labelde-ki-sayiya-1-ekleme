5f15d63224c7738c61165a9802fdbd73f81387ab
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 21:12:17 +0300

    Add files via upload

---
+++ butona basıldıgında labele 1 ekleme/butona basıldıgında labele 1 ekleme/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dizin_oluşturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int sayı = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sayı = sayı + 1;
+            label1.Text = sayı.ToString();
+            
+
+
+        }
+    }
+}
